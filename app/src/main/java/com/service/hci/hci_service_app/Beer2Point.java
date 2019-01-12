eeea811b312ee33821a616ddcf2eb14fa8b377e5
Hendrik <hendrik.scheve@haw-hamburg.de>
2019-01-12 15:13:41 +0100

    - add data_layer for communication via REST with Server(async)

---
+++ app/src/main/java/com/service/hci/hci_service_app/Beer2Point.java
@@ -0,0 +1,11 @@
+package com.service.hci.hci_service_app;
+
+import android.app.Application;
+
+
+public class Beer2Point extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
