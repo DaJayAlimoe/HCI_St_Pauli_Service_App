6f9ee7922c452013b61e7261cf1d3c58ba25f6d7
Hendrik <hendrik.scheve@haw-hamburg.de>
2019-01-12 16:02:21 +0100

    - add data_layer for communication via REST with Server(async)

--- app/src/main/java/com/service/hci/hci_service_app/MainActivity.java
+++ app/src/main/java/com/service/hci/hci_service_app/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) { // test comment
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
 
 
     }
