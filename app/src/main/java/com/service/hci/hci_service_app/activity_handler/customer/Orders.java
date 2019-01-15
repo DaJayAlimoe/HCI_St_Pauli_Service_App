d77983c179baa0fd55cdd42bc43d2cf8d41322a3
S3r4 <oliver.jenz@haw-hamburg.de>
2019-01-15 17:34:30 +0100

    PopUp

--- app/src/main/java/com/service/hci/hci_service_app/activity_handler/customer/Orders.java
+++ app/src/main/java/com/service/hci/hci_service_app/activity_handler/customer/Orders.java
@@ -1,4 +1,17 @@
 package com.service.hci.hci_service_app.activity_handler.customer;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.service.hci.hci_service_app.R;
+
 public class Orders {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.customer_orders, container, false);
+    }
 }
