4423ad09c65eb6e3dd53a0a7fb256c20f5d18af2
Ronny <dajayalimoe@ws-247-17.dynamic.haw-hamburg.de>
2019-01-16 12:55:21 +0100

    completed imageview in items

--- app/src/main/java/com/service/hci/hci_service_app/activity_handler/customer/fragments/Menu.java
+++ app/src/main/java/com/service/hci/hci_service_app/activity_handler/customer/fragments/Menu.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.service.hci.hci_service_app.R;
@@ -34,33 +33,33 @@ public class Menu extends Fragment{
 
 
 
-        Item item1 = new Item("Cola","Cola", R.drawable.rsz_beer);
-//        Item item2 = new Item("Fanta","Fanta", R.drawable.fanta);
-//        Item item3 = new Item("Sprite","Sprite", R.drawable.sprite);
-//        Item item4 = new Item("Bier","Bier", R.drawable.beer);
-//        Item item5 = new Item("Wasser","Wasser", R.drawable.water);
-//        Item item6 = new Item("brezel","Brezel", R.drawable.brezel);
-//        Item item7 = new Item("bratwurst","Bratwurst", R.drawable.bratwurst);
-//        Item item8 = new Item("Snack3","Snack3", R.drawable.cola);
-//        Item item9 = new Item("Snack4","Snack4", R.drawable.cola);
-//        Item item10 = new Item("Snack5","Snack5", R.drawable.cola);
-//        Item item11 = new Item("Snack6","Snack6", R.drawable.cola);
-//        Item item12 = new Item("Snack7","Snack7", R.drawable.cola);
-//        Item item13 = new Item("Snack8","Snack8", R.drawable.cola);
+        Item item1 = new Item("Ingredients","Cola", R.drawable.cola);
+        Item item2 = new Item("Ingredients","Fanta", R.drawable.fanta);
+        Item item3 = new Item("Ingredients","Sprite", R.drawable.sprite);
+        Item item4 = new Item("Ingredients","Bier", R.drawable.rsz_beer);
+        Item item5 = new Item("Ingredients","Wasser", R.drawable.water);
+        Item item6 = new Item("Ingredients","Brezel", R.drawable.rsz_brezel);
+        Item item7 = new Item("Ingredients","Bratwurst", R.drawable.rsz_bratwurst);
+        Item item8 = new Item("Ingredients","Snack3", R.drawable.rsz_brezel);
+        Item item9 = new Item("Ingredients","Snack4", R.drawable.rsz_brezel);
+        Item item10 = new Item("Ingredients","Snack5", R.drawable.rsz_brezel);
+        Item item11 = new Item("Ingredients","Snack6", R.drawable.rsz_brezel);
+        Item item12 = new Item("Ingredients","Snack7", R.drawable.rsz_brezel);
+        Item item13 = new Item("Ingredients","Snack8", R.drawable.rsz_brezel);
         ArrayList<Item> itemArrayList = new ArrayList<>();
         itemArrayList.add(item1);
-//        itemArrayList.add(item2);
-//        itemArrayList.add(item3);
-//        itemArrayList.add(item4);
-//        itemArrayList.add(item5);
-//        itemArrayList.add(item6);
-//        itemArrayList.add(item7);
-//        itemArrayList.add(item8);
-//        itemArrayList.add(item9);
-//        itemArrayList.add(item10);
-//        itemArrayList.add(item11);
-//        itemArrayList.add(item12);
-//        itemArrayList.add(item13);
+        itemArrayList.add(item2);
+        itemArrayList.add(item3);
+        itemArrayList.add(item4);
+        itemArrayList.add(item5);
+        itemArrayList.add(item6);
+        itemArrayList.add(item7);
+        itemArrayList.add(item8);
+        itemArrayList.add(item9);
+        itemArrayList.add(item10);
+        itemArrayList.add(item11);
+        itemArrayList.add(item12);
+        itemArrayList.add(item13);
 
         ItemListAdapter itemListAdapter = new ItemListAdapter(view.getContext() ,R.layout.customer_item_view, itemArrayList);
         listView.setAdapter(itemListAdapter);
