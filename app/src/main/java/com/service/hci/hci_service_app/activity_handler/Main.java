62e06d27925b7cef275a81d1006034d1a8cf90ac
Andre Helpap <andre.helpap@gmx.de>
2019-02-06 15:24:55 +0100

    QR-Code angefangen

--- app/src/main/java/com/service/hci/hci_service_app/activity_handler/Main.java
+++ app/src/main/java/com/service/hci/hci_service_app/activity_handler/Main.java
@@ -1,11 +1,20 @@
 package com.service.hci.hci_service_app.activity_handler;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.blikoon.qrcodescanner.QrCodeActivity;
 import com.service.hci.hci_service_app.R;
 import com.service.hci.hci_service_app.activity_handler.customer.CustomerMain;
 import com.service.hci.hci_service_app.activity_handler.customer.ItemConfirmView;
@@ -15,6 +24,12 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn_service;
     private Button btn_customer;
+    private Button btn_qr;
+
+    private static final int REQUEST_CODE_QR_SCAN = 101;
+    private final String LOGTAG = "ScanQRCode";
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // test comment
@@ -25,6 +40,8 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         btn_customer.setOnClickListener(this);
         this.btn_service = findViewById(R.id.btn_to_service);
         btn_service.setOnClickListener(this);
+        this.btn_qr = findViewById(R.id.btn_to_qr);
+        btn_qr.setOnClickListener(this);
 
         if (getIntent().getBooleanExtra("EXIT", false)) {
             Intent intent = new Intent(getApplicationContext(), Main.class);
@@ -40,11 +57,96 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         if(actView == R.id.btn_to_customer){
             Intent intent = new Intent(Main.this, CustomerMain.class);
             startActivity(intent);
-        }else if(actView == R.id.btn_to_service){
+        } else if(actView == R.id.btn_to_service){
             Intent intent = new Intent(this, ServiceMain.class);
             startActivity(intent);
+        } else if(actView == R.id.btn_to_qr){
+            // Here, thisActivity is the current activity
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+            } else {
+                Intent intent = new Intent(this, QrCodeActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_QR_SCAN);
+            }
+
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent intent = new Intent(this, QrCodeActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE_QR_SCAN);
+
+                } else {
+                    AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                    alertDialog.setTitle("Zugriffsfehler");
+                    alertDialog.setMessage("Kamerazugriff wird für QR Code Scanner benötigt");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode != Activity.RESULT_OK)
+        {
+            Log.d(LOGTAG,"COULD NOT GET A GOOD RESULT.");
+            if(data==null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image");
+            if( result!=null)
+            {
+                AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setTitle("Scan Error");
+                alertDialog.setMessage("QR Code could not be scanned");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            return;
+
+        }
+        if(requestCode == REQUEST_CODE_QR_SCAN)
+        {
+            if(data==null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
+            Log.d(LOGTAG,"QR Code scanned; result is: "+ result);
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Scan result");
+            alertDialog.setMessage(result);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+        }
     }
 }
 
